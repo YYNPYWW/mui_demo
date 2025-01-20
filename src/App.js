73a7140f054ced7ef68f8107a19fca21ee33b290
shuo.wei <yyw7042@gmail.com>
2025-01-20 18:23:47 +0800

    customizable breakpoints theme

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
 import RightBar from './components/RightBar';
 import Footer from './components/Footer';
-import { Stack } from '@mui/material';
+import { Stack, ThemeProvider } from '@mui/material';
+import theme from './theme.js';
 
 
 function App() {
   return (
-    <Stack direction='row' spacing={2} >
-      {/* <NavBar /> */}
-      <SideBar />
-      <Footer />
-      <RightBar />
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Stack direction='row' spacing={2} >
+        {/* <NavBar /> */}
+        <SideBar />
+        <Footer />
+        <RightBar />
+      </Stack>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 520
+        }
+    }
+})
+
+export default theme;
